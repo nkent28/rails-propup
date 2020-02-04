77a23a717decba5eef0aefeb96b1eecdb55cc8da
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2020-02-05 09:10:23 +1300

    coded validations for all models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :prop
   belongs_to :user
   has_one :review
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,7 @@ class Review < ApplicationRecord
   has_one :user, through: :bookings
   has_one :prop, through: :bookings
   has_many_attached :photos
-  validates :name, :content, :rating, presence: true
-end
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
