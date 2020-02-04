cd1739cc6e8cda8db71a90d5ab8c08e8f6828861
Zoe Campbell <z.t.campbell1@gmail.com>
2020-02-04 21:45:04 +0000

    coded bookings controller actions, but not alla dem!

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,50 @@
 class BookingsController < ApplicationController
+
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def show
+
+  end
+
+  def new
+    @prop = Find(params[:prop_id])
+    @booking = Booking.new
+  end
+
+  def create
+
+    @booking = Booking.new(booking_params)
+    @booking.create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_prop
+    @booking = Booking.find(params[:id])
+  end
 end
